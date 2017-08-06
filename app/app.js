175b37a3aa53fa5b8d608dab12fc61d7c1267fce
Apoorva Saxena <apsaxena24@gmail.com>
2017-08-06 15:36:03 +0100

    more style

--- app/app.js
+++ app/app.js
@@ -1,18 +1,21 @@
 (function() {
     var app = angular.module("FlickrApp", []);
-
+    app.filter('unsafe', function($sce) { return $sce.trustAsHtml; });
     app.controller('FeedController', function($http, $scope) {
         return $http({
             method: 'GET',
             url: 'http://localhost:8080/api/feeds',
         }).then(function successCallback(response) {
-        	// const feeds_data = toJson(response.data)
-        	console.log("I reach here")
-        	console.log(response.data.items )
-        	$scope.feeds = response.data.items
+            $scope.feeds = response.data.items
         }, function errorCallback(response) {
             console.log("error: ", response)
         });
+
+        // $scope.renderHtml = function(html_code) {
+        // 	console.log(html_code)
+        //     return $sce.trustAsHtml(html_code);
+        // };
+
     })
 
 
